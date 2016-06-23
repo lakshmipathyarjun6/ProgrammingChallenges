1e785a7e160a3c8ea4bc99cd89cd78ead074f264
Arjun Lakshmipathy <lakshmipathyarjun6@gmail.com>
2016-06-23 11:29:38 -0700

    Some challenge solutions added

---
+++ LeetCode/UniqueBinarySearchTreesII/UniqueBinarySearchTreesII.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0){
+            return new ArrayList<TreeNode>();
+        }
+        
+        return helper(1, n);
+    }
+    
+    public List<TreeNode> helper(int m, int n) {
+        List<TreeNode> result = new ArrayList<TreeNode>();
+        if(m>n){
+            result.add(null);
+            return result;
+        }
+        
+        for(int i=m; i<=n; i++){
+            List<TreeNode> ls = helper(m, i-1);
+            List<TreeNode> rs = helper(i+1, n);
+            for(TreeNode l: ls){
+                for(TreeNode r: rs){
+                    TreeNode curr = new TreeNode(i);
+                    curr.left=l;
+                    curr.right=r;
+                    result.add(curr);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
