e343557cb4b63d3375531a7decffd2e6c82cf69d
Arjun Lakshmipathy <lakshmipathyarjun6@gmail.com>
2016-08-09 01:21:50 -0700

    Code edit

--- LeetCode/Valid_Palindrome/Valid_Palindrome.java
+++ LeetCode/Valid_Palindrome/Valid_Palindrome.java
@@ -1,7 +1,6 @@
 public class Solution {
     public boolean isPalindrome(String s) {
         String newS = s.toLowerCase().replaceAll("\\s+","").replaceAll("[^A-Za-z0-9 ]", "");
-        System.out.println(newS);
         for(int i = 0; i < newS.length()/2; i++) {
             if(newS.charAt(i) != newS.charAt(newS.length()-i-1)) {
                 return false;
